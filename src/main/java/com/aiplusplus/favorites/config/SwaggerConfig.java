921ff03ebd9faf6d97760624bfceab8d76c4a90b
李俊杰 <aiPlusPlus948@163.com>
2024-03-04 17:53:49 +0800

    swagger全局参数

--- src/main/java/com/aiplusplus/favorites/config/SwaggerConfig.java
+++ src/main/java/com/aiplusplus/favorites/config/SwaggerConfig.java
@@ -2,9 +2,14 @@ package com.aiplusplus.favorites.config;
 
 
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+
 
 
 /**
@@ -23,5 +28,17 @@ public class SwaggerConfig {
                 .group("spring-projects-favorites")
                 .packagesToScan("com.aiplusplus.favorites")
                 .build();
+    }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API favorites").version("1.0").description("API favorites项目"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
